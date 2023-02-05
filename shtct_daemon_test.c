4d747383d5d7b0332e5179b5ff0af35bf5c020d3
Typeaway14 <harsh.som33@gmail.com>
2023-02-05 15:30:00 +0530

    removed all unnecesarry code

--- shtct_daemon_test.c
+++ shtct_daemon_test.c
@@ -6,22 +6,12 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 #include<linux/input.h>//structure definition of input_event is here
 #include<unistd.h>//read()
 #include<fcntl.h>//has open() and O_RDONLY macro
 #include<errno.h>//for errno and EINTR. Look into these and what these mean
-#include<time.h>//for struct tm and other tiem related functions. GET BETTER UNDERSTANDING
 
-typedef struct keycodes_structure
-{
-    char key_name[12];
-    int key_code;
-}KSTR;
-
-void date_and_time(char*);
-int kbd_read(KSTR*);
-int key_detect(int* key_code, ssize_t* n,int* fd,struct input_event* ev, KSTR* kbd_array, int recurse);//function containing only loop part to make recursion possible
+int key_detect(int* key_code, ssize_t* n,int* fd,struct input_event* ev, int recurse);//function containing only loop part to make recursion possible
 int keyboard_listener(int* activation_key_code);
 
 int main()
@@ -51,14 +41,11 @@ int keyboard_listener(int* activation_key_codes)
     struct input_event ev;
     int fd;
     ssize_t n;
-    KSTR kbd_array[85];
-    int kbd_array_status;
     int key_detect_return;
     int recurse = 0;
 
-    kbd_array_status = kbd_read(kbd_array);
     fd = open(kbd_path,O_RDONLY);
-    if(fd == -1 || !kbd_array_status)
+    if(fd == -1)
     {
         printf("Failed to open required files. Terminating.\n");
         exit(0);
@@ -68,17 +55,15 @@ int keyboard_listener(int* activation_key_codes)
         recurse = 1;
     }
     
-    key_detect_return = key_detect(activation_key_codes,&n,&fd,&ev,kbd_array,recurse);
+    key_detect_return = key_detect(activation_key_codes,&n,&fd,&ev,recurse);
     close(fd);
     return key_detect_return;
 }
 
-int key_detect(int* activation_key_codes, ssize_t* n,int* fd,struct input_event* ev, KSTR* kbd_array, int recurse)
+int key_detect(int* activation_key_codes, ssize_t* n,int* fd,struct input_event* ev, int recurse)
 {
-    char timenow[30];
     while(1)
     {
-        fflush(stdin);
         *n = read(*fd, ev, sizeof(struct input_event));
         if(*n == (ssize_t)-1)
         {
@@ -100,7 +85,7 @@ int key_detect(int* activation_key_codes, ssize_t* n,int* fd,struct input_event*
                     recurse = 0;
                 }
                 printf("\n\nI am now going to call the same function \n");    
-                return key_detect(activation_key_codes+1,n,fd,ev,kbd_array,recurse);
+                return key_detect(activation_key_codes+1,n,fd,ev,recurse);
             }
             else if((*ev).code == activation_key_codes[0] && recurse ==0 && ((*ev).value == 1 || (*ev).value == 2))
             {    
@@ -109,24 +94,4 @@ int key_detect(int* activation_key_codes, ssize_t* n,int* fd,struct input_event*
             }
         }
     }
-}
-
-int kbd_read(KSTR* kbd_array)
-{
-    FILE* key_codes_fp = fopen("c_keycodes.csv","r");
-    if(!key_codes_fp)
-    {
-        fclose(key_codes_fp);
-        return 0;
-    }
-    char buffer[700];
-    int row=0;
-    while(fgets(buffer,700,key_codes_fp))
-    {
-        strcpy(kbd_array[row].key_name,strtok(buffer,","));
-        kbd_array[row].key_code = atoi(strtok(NULL,","));
-        row++;
-    }
-    fclose(key_codes_fp);
-    return 1;
 }
